92d5877ce3a86f3dd063cae652b2bf9994e1cb0d
danfyfe <fyfedaniel@gmail.com>
2019-11-05 16:10:42 -0500

    flipping menu

--- src/containers/FunContainer.tsx
+++ src/containers/FunContainer.tsx
@@ -3,9 +3,10 @@ import React from 'react'
 const FunContainer: React.FC = props => {
   return(
     <div className='fun mx-auto my-4'>
-      <div className=''>
-
-      </div>
+    <div className='bar-container'>
+      <div className='bar'/>
+      <div className='bar'/>
+    </div>
     </div>
   )
 }
