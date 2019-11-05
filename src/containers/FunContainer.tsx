2ea6e45c2884b473ef03a6f651c1f0d719074582
danfyfe <fyfedaniel@gmail.com>
2019-11-05 16:24:55 -0500

    transform

--- src/containers/FunContainer.tsx
+++ src/containers/FunContainer.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
 const FunContainer: React.FC = props => {
+
+  const changeBars = () => {
+    const top = document.querySelector('#top')
+    const mid = document.querySelector('#mid')
+    const bot = document.querySelector('#bot')
+
+    if( top && mid && bot ) {
+      top.className = 'right'
+      bot.className = 'left'
+      mid.className = 'mid'
+    }
+  };
+
   return(
     <div className='fun mx-auto my-4'>
-    <div className='bar-container'>
-      <div className='bar'/>
-      <div className='bar'/>
+    <div className='bar-container' onClick={changeBars}>
+      <div className='bar' id='top'/>
+      <div className='bar' id='mid'/>
+      <div className='bar' id='bot'/>
     </div>
     </div>
   )
